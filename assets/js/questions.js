b9518ab80016fff10d3a9ea80659541acdbd8bc2
ik <59951030+i-k-dev@users.noreply.github.com>
2023-01-19 02:43:42 +0000

    unable to fix js code, questions doesnt seem to want to pop up, give up

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        title: "Unrelated JavaScript fundamental question: does Quentin Tarantino like feet?",
+        choice: ["Yes", "No", "Probably", "Everyone likes feet!"],
+        answer: "Yes"
+    },
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choice: ["<script>", "<js>", "<scripting>", "<javscript>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "The external JavaScript file must contain the <script> tag...",
+        choice: ["True", "False"],
+        answer: "False"
+    },
+
+    {
+        title: ['How do you write "HELLO WORLD" in an alert box?'],
+        choice: ["alert(text-here)", "msg(text-here)", "msgBox(text-here)", "alertBox(text-here)"],
+        answer: "alert(text-here)"
+    },
+
+    {
+        title: "Is JavaScript the same as Java?",
+        choice: ["No it isn't", "Duh, YEAH???"],
+        answer: "No it isn't"
+    },
+
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choice: ["onmouseclick", "onmouseover", "onclcik", "onchange"],
+        answer: "onclick"
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,12 @@
-let start = document.getElementById('#start')
-let timer = document.getElementById('#timer')
-let questions = document.getElementById('#questions')
-let Highscores = document.getElementById('#scores')
+let timerStart = 100;
+let timer = document.getElementById("timer");
+let questions = document.getElementById("questions");
+let scoresDiv = document.getElementById("scores");
+let buttonsDiv = document.getElementById("button")
+let viewScoresBtn = document.getElementById("view-scores")
+let startButton = document.getElementById("button-start");
+startButton.addEventListener("click", setTime);
 
-let timerCountdown;
-let seconds;
-let quiz;
-let quizQuestions;
-let quizAnswers;
 
-const jsQuestions = [
-    'Inside which HTML element do we put the JavaScript?',
-    'The external JavaScript file must contain the <script> tag',
-    'How do you write "HELLO WORLD" in an alert box?',
-    'JavaScript is the same as Java?',
-    'Which event occurs when the user clicks on an HTML element?'
-];
-
-const jsAnswers = [
-    [['<script>', true]], [['<js>', false]], [['<scripting>', false]], [['<javscript>', false]],
-    [['True', false]], [['False', true]],
-    [['alert("HELLO WORLD")', true]], [['msg("HELLO WORLD")', false]], [['msgBox("HELLO WORLD")', false]], [['alertBox("HELLO WORLD")', false]],
-    [['No it isnt', true]], [['Duh, Yeah??', false]],
-    [['onmouseclick', false]], [['onmouseover', false]], [['onclick', true]], [['onchange', false]],
-];
+var questionDiv = document.getElementById("questions");
+// its not working?
